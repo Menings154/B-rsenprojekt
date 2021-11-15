8da98d75809c9c35f55f4b057b9082b319026479
Menings154 <menings.git@gmail.com>
2021-11-15 07:28:08 -0500

    list for crv_tickers included

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from googlesearch import search
 import requests
 from bs4 import BeautifulSoup
 
-print('Start')
+
 # load the different indexes and scrap for tickers to later iterate over the tickers
 sandp = Sandp500()
 sandp.scrap_tickers()
@@ -21,9 +21,11 @@ for ticker in sandp.tickers:
         pass
 
 crv_names = []
+crv_tickers = []
 for ticker in dic_crv.keys():
     index = sandp.tickers.index(ticker)
     crv_names.append(sandp.names[index])
+    crv_tickers.append(ticker)
 
 # Ersten 5 Googleergebnisse als links speichern und dann per Email senden
 links = []
@@ -35,7 +37,7 @@ for count, name in enumerate(crv_names):
 # Write the mail
 body = f'Here are todays recomendations:\r'
 for count, name in enumerate(crv_names):
-    body += f'{name}: CRV = {dic_crv[dic_crv.keys()[count]]}\r'
+    body += f'{name}: CRV = {dic_crv[crv_tickers[count]]}\r'
     for i in links[count]:
         body += f'{i}\r'
 body += '\r'
